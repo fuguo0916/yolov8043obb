9a88b64e739d599d6407be00bc8527cdb0d2f00e
tmm <tmm7569559@outlook.com>
2023-03-20 16:38:22 +0800

    at v1.0: update .gitignore and ready to merge fv

--- train_03_att.py
+++ train_03_att.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         model = YOLO(f"ultralytics/models/v8/yolov8s_{name}.yaml")
         model.train(**{"cfg": f"ultralytics/yolo/cfg/train.yaml",
                     "project": f"runs/train_03_att",
-                    "name": f"train_e1000_iou3_b16_i960_valriou_{name}_close100",
+                    "name": f"train_e1000_iou3_b16_i960_valriou_{name}_close50",
                     "epochs": 1000,
                     "batch": 16,
                     "imgsz": 960,
