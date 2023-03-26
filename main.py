8fe02f4086da7d7463f9935306e49bc55ef2aae9
tmm <tmm7569559@outlook.com>
2023-03-26 19:17:02 +0800

    begin test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         model = YOLO("runs/train_02_epochs_batch_imgsz/train_e500_iou3_b4_i1280_valriou_noatt_close20/weights/last.pt")
         model.val(imgsz=1280, save_json=True, iou=0.3, batch=4)
     elif sys.argv[1] == "test":
-        model = YOLO("runs/detect/train9/weights/last.pt")
-        model.predict(source="E:\\data\\aiprj\\yolov8043obb\\mydata\\test.txt", conf=0.25, iou=0.5, save_txt=True, save=True)
+        model = YOLO("runs/train_08_miou/train_e1000_iou3_b16_i640_miou_valriou_noatt_close200/weights/last.pt")
+        model.predict(source="/root/autodl-tmp/yolov8043obb/mydata/test.mp4", conf=0.1, iou=0.3, save_txt=True, save=True)
     else:
         assert False, f"Wrong argument to main.py"
--- train_08_miou.py
+++ train_08_miou.py
@@ -6,7 +6,7 @@ os.system(f"echo train_e1000_iou3_b16_i640_miou_valriou_noatt_close200 >> whispe
 model = YOLO(f"ultralytics/models/v8/yolov8s_noatt.yaml")
 model.train(**{"cfg": f"ultralytics/yolo/cfg/train.yaml",
             "project": f"runs/train_08_miou",
-            "name": f"train_e1000_iou3_b16_i640_miou_valriou_noatt_close200",
+            "name": f"trainl_e1000_iou3_b16_i640_miou_valriou_noatt_close200",
             "epochs": 1000,
             "batch": 16,
             "imgsz": 640,
